2378807c698a9446027e970134b34a13f479ad8e
Erik Blankinship <erikb@mediamods.com>
2024-01-23 15:20:09 -0500

    more

--- app/dgmd_types.js
+++ app/dgmd_types.js
@@ -1,46 +1,41 @@
+export const DGMD_BLOCK_TYPE_CHECKBOX = 'checkbox';
+export const DGMD_BLOCK_TYPE_COVER = 'cover';
 export const DGMD_BLOCK_TYPE_CREATED_TIME = 'created_time';
-export const DGMD_BLOCK_TYPE_LAST_EDITED_TIME = 'last_edited_time'
+export const DGMD_BLOCK_TYPE_DATE = 'date';
 export const DGMD_BLOCK_TYPE_EMAIL = 'email';
+export const DGMD_BLOCK_TYPE_EMOJI = 'emoji';
+export const DGMD_BLOCK_TYPE_FILE_EXTERNAL = 'external';
+export const DGMD_BLOCK_TYPE_FORMULA = 'formula';
+export const DGMD_BLOCK_TYPE_ICON = 'icon';
+export const DGMD_BLOCK_TYPE_ID = 'id';
+export const DGMD_BLOCKS = 'DGMD_BLOCKS';
+export const DGMD_BLOCK_TYPE_LAST_EDITED_TIME = 'last_edited_time'
+export const DGMD_BLOCK_TYPE_MULTI_SELECT = 'multi_select';
+export const DGMD_BLOCK_TYPE_NUMBER = 'number';
 export const DGMD_BLOCK_TYPE_PHONE_NUMBER = 'phone_number';
-export const DGMD_BLOCK_TYPE_URL = 'url';
+export const DGMD_BLOCK_TYPE_RELATION = 'relation';
+export const DGMD_BLOCK_TYPE_RICH_TEXT = 'rich_text';
 export const DGMD_BLOCK_TYPE_SELECT = 'select';
 export const DGMD_BLOCK_TYPE_STATUS = 'status';
 export const DGMD_BLOCK_TYPE_TITLE = 'title';
-export const DGMD_BLOCK_TYPE_RICH_TEXT = 'rich_text';
-export const DGMD_BLOCK_TYPE_NUMBER = 'number';
-export const DGMD_BLOCK_TYPE_MULTI_SELECT = 'multi_select';
-export const DGMD_BLOCK_TYPE_CHECKBOX = 'checkbox';
-export const DGMD_BLOCK_TYPE_DATE = 'date';
-export const DGMD_BLOCK_TYPE_EMOJI = 'emoji';
-export const DGMD_BLOCK_TYPE_FILE_EXTERNAL = 'external';
-export const DGMD_BLOCK_TYPE_RELATION = 'relation';
-export const DGMD_BLOCK_TYPE_ICON = 'icon';
-export const DGMD_BLOCK_TYPE_COVER = 'cover';
-export const DGMD_BLOCK_TYPE_FORMULA = 'formula';
 
-
-export const DGMD_PRIMARY_DATABASE = 'DGMD_PRIMARY_DATABASE';
-export const DGMD_RELATION_DATABASES = 'DGMD_RELATION_DATABASES';
-export const DGMD_DATABASE_ID = 'DGMD_DATABASE_ID';
-export const DGMD_DATABASE_TITLE = 'DGMD_DATABASE_TITLE';
-export const DGMD_PARENT_TITLE = 'DGMD_PARENT_TITLE';
-export const DGMD_PARENT_ID = 'DGMD_PARENT_ID';
-export const DGMD_PARENT_TYPE = 'DGMD_PARENT_TYPE';
-export const DGMD_BLOCKS = 'DGMD_BLOCKS';
+export const DGMD_BLOCK_TYPE_URL = 'url';
 export const DGMD_COLUMN_LISTS = 'DGMD_COLUMN_LISTS';
 export const DGMD_CURSOR_DATA = 'DGMD_CURSOR_DATA';
-
-export const DGMD_PAGE_DATA = 'DGMD_PAGE_DATA';
-
-export const DGMD_START_DATE = 'DGMD_START_DATE';
+export const DGMD_DATABASE_ID = 'DGMD_DATABASE_ID';
+export const DGMD_DATABASE_TITLE = 'DGMD_DATABASE_TITLE';
 export const DGMD_END_DATE = 'DGMD_END_DATE';
+export const DGMD_KEY = 'KEY';
+export const DGMD_METADATA = 'METADATA';
+export const DGMD_PAGE_DATA = 'DGMD_PAGE_DATA';
+export const DGMD_PARENT_ID = 'DGMD_PARENT_ID';
+export const DGMD_PARENT_TITLE = 'DGMD_PARENT_TITLE';
+export const DGMD_PARENT_TYPE = 'DGMD_PARENT_TYPE';
+export const DGMD_PRIMARY_DATABASE = 'DGMD_PRIMARY_DATABASE';
+export const DGMD_PROPERTIES = 'PROPERTIES';
+export const DGMD_RELATION_DATABASES = 'DGMD_RELATION_DATABASES';
+export const DGMD_START_DATE = 'DGMD_START_DATE';
 export const DGMD_TIME_ZONE = 'DGMD_TIME_ZONE';
-
 export const DGMD_TYPE = 'TYPE';
 export const DGMD_VALUE = 'VALUE';
-export const DGMD_KEY = 'KEY';
-export const DGMD_PROPERTIES = 'PROPERTIES';
-export const DGMD_METADATA = 'METADATA';
 
-export const DGMD_BLOCK_ID = 'BLOCK_ID';
-export const DGMD_PAGE_ID = 'PAGE_ID';
