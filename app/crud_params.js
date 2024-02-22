33763e36e74057c6b353720ef5ebb51c5b3d37fb
Erik Blankinship <erikb@mediamods.com>
2024-02-22 13:27:35 -0500

    refactor

--- app/crud_params.js
+++ app/crud_params.js
@@ -9,6 +9,3 @@ export const CRUD_PARAM_UPDATE_BLOCK = 'ub';
 export const CRUD_PARAM_UPDATE_BLOCK_ID = 'ubi';
 
 export const CRUD_PARAM_UPDATE_META = 'um';
-export const CRUD_VALUE_ACTION_CREATE = 'c';
-export const CRUD_VALUE_ACTION_DELETE = 'd';
-export const CRUD_VALUE_ACTION_UPDATE = 'u';
